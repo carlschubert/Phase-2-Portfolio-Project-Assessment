b0c18454ec46f09caf94b3ebd335fcb9a67f85ee
carlschubert <carl.schubert@gmail.com>
2015-01-07 12:02:02 -0500

    add button jquery to run game logic

--- public/js/application.js
+++ public/js/application.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
   var match = new Match()
   match.popGames()
 
-
-
-
-});
+  $('.choice').click(function() {
+      userChoice = $(this).attr('id')
+      match.playGame(userChoice)
+      match.popGames()
+    })
+})
