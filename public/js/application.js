4b25e9a9734c4e970496f94b02d2658b5cdc9599
carlschubert <carl.schubert@gmail.com>
2015-01-07 19:40:46 -0500

    add ajax for try again button, needs refactor

--- public/js/application.js
+++ public/js/application.js
@@ -10,4 +10,31 @@ $(document).ready(function() {
       match.checkMatchWinner()
       match.popGames()
     })
+
+
+  $(".content").on("click", '#new_game_button', function(event){
+     event.preventDefault()
+     var $target = $(event.target)
+     $.ajax({
+       url: $target.attr('action'),
+       type: 'POST',
+     }).always(function(){
+       match.resetMatch()
+       match.popGames()
+     }).done(function(response){
+       $('.content').empty().append(response)
+       $('.choice').click(function() {
+          userChoice = $(this).attr('id')
+          match.playGame(userChoice)
+          match.checkGameWinner()
+          match.checkMatchWinner()
+          match.popGames()
+        })
+     }).fail(function(){
+       console.log('failed')
+     })
+
+   })
+
+
 })
