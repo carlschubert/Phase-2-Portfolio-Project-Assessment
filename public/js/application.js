2f668d456dd7c661ff9d93d4cc7ebb6fc076a4f0
carlschubert <carl.schubert@gmail.com>
2015-01-07 21:02:56 -0500

    add ajax for login

--- public/js/application.js
+++ public/js/application.js
@@ -39,4 +39,30 @@ $(document).ready(function() {
    })
 
 
+  $('#login_button').on('click', function (event) {
+    event.preventDefault()
+    var $target = $(event.target)
+    $.ajax({
+      url: '/login',
+      type: 'GET',
+    }).done(function (response) {
+      $target.closest('button').replaceWith(response)
+    })
+
+  })
+
+    $('#signup_button').on('click', function (event) {
+    event.preventDefault()
+    var $target = $(event.target)
+
+    $.ajax({
+      url: '/signup',
+      type: 'GET',
+    }).done(function (response) {
+      console.log('hi')
+      $target.closest('button').replaceWith(response)
+    })
+  })
+
+
 })
